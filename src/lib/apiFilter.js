ba30ee8317aa55031d56e268c2f3605406b77f8b
MichaelDimmitt <michaelgdimmitt@gmail.com>
2019-07-06 17:41:58 -0400

    sort equivalents ... for underscore because it improves readability.

--- src/lib/apiFilter.js
+++ src/lib/apiFilter.js
@@ -1,42 +1,51 @@
 import _ from 'underscore';
+
 const sortCompanies = (companies, key) => {
     return _.sortBy(companies, key);
+    // companies.sort(a, b => a[key] - b[key])
 }
+
 const sortCompaniesReverse = (companies, key) => {
     return _.sortBy(companies, key).reverse();
+    // companies.sort(a, b => b[key] - a[key])
 }
+
 const sort = (companies, key, direction) => {
     return direction === 'ascend'
         ? sortCompanies(companies, key)
         : direction === 'descend'
             ? sortCompaniesReverse(companies, key)
             : companies;
-
 }
+
 const search = (companies, keyArr, query) => {
     if (!query){return companies}
-    return _.filter(companies,
-        (obj) => {
-            const result = keyArr.filter(key => (
-                obj[key].toLowerCase().includes(query.toLowerCase())
-            ))
-            return result.length > 0
-        }
-    )
+    return companies.filter( company => {
+        const result = keyArr.filter(key => (
+            obj[key].toLowerCase().includes(query.toLowerCase())
+        ));
+        return result.length > 0;
+//      return orLogic(companiesWithKeys.length, keyArr.length)
+    }
 }
+
 const filterCompanies = (companies, keyArr) => {
-    return _.filter(companies,
-        (obj) => {
-            const result = keyArr.filter(key => (
-                obj[key] ? true : false
-            ))
-            return result.length === keyArr.length
+    return companies.filter(
+        (company) => {
+            const companiesWithKeys = keyArr.filter(key =>
+                    company[key] ? true : false
+                  )
+            return companiesWithKeys.length === keyArr.length;
+//          return andLogic(companiesWithKeys.length, keyArr.length)
         }
     )
-}
+};
+
 const reject = (companies, stringList) => {
     return _.reject(companies, (company) => {
         return stringList.includes(company.symbol);
-    })
+    });
+// return companies.filter(company => !stringList.includes(company.symbol) );
 }
+
 export { sort, search, filterCompanies, reject }
